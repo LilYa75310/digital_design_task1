41c6f7db52a6cc99784fc892b40e34d4c7b94e76
LilYa <LilYa75310@gmail.com>
2025-10-20 14:20:06 +0300

    [feat] added logic for display

--- task_4/sources/rtl/alu_top.sv
+++ task_4/sources/rtl/alu_top.sv
@@ -1,13 +1,15 @@
 module alu_top #(
     parameter purpouse  = "synthesis", // set to "simulate" for simulation and "synthesis" for implementation
     parameter WIDTH_COUNTER_DEBOUNCE = 24,
-    parameter CLOCK_FREQ_DEBOUNCE = 50_000_000,
+    parameter CLOCK_FREQ_DEBOUNCE = 10_000_000,
     parameter STABLE_TIME_MS_DEBOUNCE = 20,
     parameter WIDTH_COUNTER = 8,
     parameter WIDTH_MUX = 16,
-    parameter STEP_COUNTER = 10,
+    parameter STEP_COUNTER = 1,
     parameter PERIOD_BIT_PWM = 12,
-    parameter DUTY_WIDTH_PWM = 8) 
+    parameter DUTY_WIDTH_PWM = 8,
+    parameter DISPLAY_CLK_DIV = 1000
+    ) 
 (  
     // input Clock & Reset
     input logic sys_clk_200_p,
@@ -19,10 +21,10 @@ module alu_top #(
     input logic sw2_up,
     input logic sw2_down,
     input logic [2:0] op_sw,
-    // RGB leds
-    output logic		led0_pwm_r,
-    output logic		led0_pwm_g,
-    output logic		led0_pwm_b
+    input logic [2:0] num_select,
+    //I2C
+    inout  sda_io,
+    output seg_scl_o
 );
     genvar i;
     logic clk_100;
@@ -30,12 +32,12 @@ module alu_top #(
     logic [WIDTH_COUNTER-1:0] counter_2;
     logic [WIDTH_MUX-1:0] mux_output;
     //logic [WIDTH_COUNTER-1:0] mux_output_scaled;
-    logic[6:0] switches;
-    logic[6:0] switches_debounced; //wire
+    logic[9:0] switches;
+    logic[9:0] switches_debounced; //wire
     logic sw2_down_d, sw2_up_d, sw1_down_d, sw1_up_d;
     logic [2:0] op_sw_d;
-    logic [2:0] pwm_o;
-    logic [2:0][7:0] duty;
+    logic [2:0] num_select_d;
+    logic [3:0][3:0]data_disp;
 
     // alias op_sw_d     = switches_debounced[4];
     // alias sw2_down_d  = switches_debounced[3];
@@ -43,13 +45,13 @@ module alu_top #(
     // alias sw1_down_d  = switches_debounced[1];
     // alias sw1_up_d    = switches_debounced[0];
     
-    assign switches = {op_sw[2:0], sw2_down, sw2_up, sw1_down, sw1_up};
-    
-    assign op_sw_d     = switches_debounced[6:4];
-    assign sw2_down_d  = switches_debounced[3];
-    assign sw2_up_d    = switches_debounced[2];
-    assign sw1_down_d  = switches_debounced[1];
-    assign sw1_up_d    = switches_debounced[0];
+    assign switches = {num_select[2:0], op_sw[2:0], sw2_down, sw2_up, sw1_down, sw1_up};
+    assign num_select_d = switches_debounced[9:7];
+    assign op_sw_d      = switches_debounced[6:4];
+    assign sw2_down_d   = switches_debounced[3];
+    assign sw2_up_d     = switches_debounced[2];
+    assign sw1_down_d   = switches_debounced[1];
+    assign sw1_up_d     = switches_debounced[0];
 
     // Instantiation of the clocking network
     //--------------------------------------
@@ -66,7 +68,7 @@ module alu_top #(
     //--------------------------------------
 
     generate
-        for(i = 0; i < 7; i++) begin
+        for(i = 0; i < 10; i++) begin
             debounce_sw #(
                 .WIDTH_COUNTER(WIDTH_COUNTER_DEBOUNCE),
                 .CLOCK_FREQ(CLOCK_FREQ_DEBOUNCE),
@@ -114,43 +116,47 @@ module alu_top #(
         .alu_result(mux_output)
     );
 
-    //--------------------------------------
-    //Instantiation of scaler
-    //--------------------------------------
-    // scaler_n_to_p_bit #(
-    //     .NBIT(WIDTH_COUNTER),
-    //     .PBIT(DUTY_WIDTH_PWM)
-    // ) scaler (
-    //     .num_i(mux_output),
-    //     .result_o(mux_output_scaled)
-    // );
-    //--------------------------------------
-
-    //Instantiation of PWM
-    //--------------------------------------
-
-    assign duty[0] = {mux_output[5:0], 2'b0};
-    assign duty[1] = {mux_output[10:6], 3'b0};
-    assign duty[2] = {mux_output[15:11], 3'b0};
-
-    generate
-        for(i = 0; i < 3; i++) begin
-            pwm #(
-                .WIDTH(PERIOD_BIT_PWM),
-                .DUTY_WIDTH(DUTY_WIDTH_PWM)
-            ) pwm_i (
-                .clk_i(clk_100),
-                .reset_ni(reset_sw_n),
-                .enable_ni(1'b1),
-                .duty_i(duty[i]), //.duty_i(mux_output_scaled),
-                .period_i(12'd4095),
-                .pwm_o(pwm_o[i])
-    );
-        end
-    endgenerate
     
-    assign {led0_pwm_r, led0_pwm_g, led0_pwm_b} = pwm_o;
-    //--------------------------------------
+    always_comb begin
+        case (num_select_d)
+            3'b001: begin
+                data_disp[3] = counter_1[3:0];
+                data_disp[2] = counter_1[7:4];
+                data_disp[1] = '0;
+                data_disp[0] = '0;
+            end
 
+            3'b010: begin
+                data_disp[3] = counter_2[3:0];
+                data_disp[2] = counter_2[7:4];
+                data_disp[1] = '0;
+                data_disp[0] = '0;
+            end
+
+            3'b100: begin
+                data_disp[3] = mux_output[3:0];
+                data_disp[2] = mux_output[7:4];
+                data_disp[1] = mux_output[11:8];
+                data_disp[0] = mux_output[15:12];
+            end
+
+            default: begin
+                data_disp[3] = mux_output[3:0];
+                data_disp[2] = mux_output[7:4];
+                data_disp[1] = mux_output[11:8];
+                data_disp[0] = mux_output[15:12];
+            end
+        endcase
+    end
+
+    display #(
+        .DISPLAY_CLK_DIV(DISPLAY_CLK_DIV)
+    ) display_7 (
+        .clk_i(clk_100),
+        .porb_i(reset_sw_n),
+        .data_disp(data_disp),
+        .sda_io(sda_io),
+        .seg_scl_o(seg_scl_o)
+    );
 
 endmodule
